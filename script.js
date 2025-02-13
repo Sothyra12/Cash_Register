bf54f67bcce301380247951b280bec7515069a60
Sothyra12 <schan256@my.centennialcollege.ca>
2025-02-12 23:38:35 -0500

    added elements html file and functions to js file

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // script.js
 
+const cashInput = document.getElementById("cash");
+const changeDueElement = document.getElementById("change-due");
+const purchaseBtn = document.getElementById("purchase-btn");
+
+let cash = 0;
 let price = 1.87;
 let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
 ];
+
+const checkForChangeDue = () => {
+  if (cashInput.value < price) {
+    alert("Customer does not have enough money to purchase the item");
+  } else if ((cash = cashInput.value)) {
+    changeDueElement.textContent =
+      "No change due - customer paid with exact cash";
+  } else {
+    calculateChangeDue();
+  }
+};
+
+const calculateChangeDue = () => {};
+
+purchaseBtn.addEventListener("click", () => {
+  checkForChangeDue();
+});
